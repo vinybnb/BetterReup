41e4e0d310a55520b29eba1c3cda13a53e5ad8cc
Mickey Ngo <nanyangbk@gmail.com>
2019-01-26 01:33:48 +0700

    upload video

--- BetterReup/Helpers/Configs.cs
+++ BetterReup/Helpers/Configs.cs
@@ -8,11 +8,16 @@ namespace BetterReup.Helpers
 {
     class Configs
     {
+        public int Mode { get; set; }
         public int Cut_Second_Min { get; set; }
         public int Cut_Second_Max { get; set; }
         public int Start { get; set; }
         public int Num_Videos { get; set; }
         public int Concurrent { get; set; }
+        public int Page_Load { get; set; }
+        public int Dialog_Load { get; set; }
         public string Channel_Id { get; set; }
+        public string Profile { get; set; }
+        public string Video_Path { get; set; }
     }
 }
--- BetterReup/Helpers/VideoHelper.cs
+++ BetterReup/Helpers/VideoHelper.cs
@@ -1,9 +1,12 @@
 ﻿using Newtonsoft.Json;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using YoutubeExplode;
 using YoutubeExplode.Converter;
@@ -70,13 +73,56 @@ namespace BetterReup.Helpers
             }
         }
 
-        public async Task<bool> UploadVideo(Video video)
+        public bool UploadVideo(Video video)
         {
+            ChromeDriver driver = null;
+            var status = false;
             try
             {
+                string profile_path = Path.GetDirectoryName(config.Profile);
+                string profile_name = Path.GetFileName(config.Profile);
+                ChromeOptions options = new ChromeOptions();
+                options.AddArguments("--user-data-dir=" + profile_path);
+                options.AddArguments("--profile-directory=" + profile_name);
+                options.AddArguments("start-maximized");
+                options.AddArguments("disable-infobars");
+                if (config.Mode == 0)
+                {
+                    options.AddArguments("--headless");
+                }
+                driver = new ChromeDriver(options);
+                driver.Navigate().GoToUrl("https://www.youtube.com/upload");
+                Thread.Sleep(config.Page_Load);
+                //var uploadButton = driver.FindElementsById("start-upload-button-single").First(x => x.Displayed);
+                var uploadButton = driver.FindElement(By.XPath("//*/div[@id='upload-prompt-box']/div[2]"));
+                uploadButton.Click();
+                Thread.Sleep(config.Dialog_Load);
+                System.Windows.Forms.SendKeys.SendWait(config.Video_Path + $"{video.Id}_cut.mp4");
+                System.Windows.Forms.SendKeys.SendWait("{Enter}");
+                Thread.Sleep(config.Page_Load);
 
+                var titleInput = driver.FindElement(By.XPath("//*/input[@class='yt-uix-form-input-text video-settings-title']"));
+                titleInput.SendKeys(Keys.Control + "a");
+                System.Windows.Forms.Clipboard.SetText(video.Title + " #1");
+                titleInput.SendKeys(Keys.Control + "v");
 
-                return true;
+                var descriptionInput = driver.FindElement(By.XPath("//*/textarea[@class='yt-uix-form-input-textarea video-settings-description']"));
+                System.Windows.Forms.Clipboard.SetText(video.Description);
+                descriptionInput.SendKeys(OpenQA.Selenium.Keys.Control + "v");
+
+                var tagInput = driver.FindElement(By.XPath("//*/input[@class='video-settings-add-tag']"));
+                foreach (var tag in video.Keywords)
+                {
+                    System.Windows.Forms.Clipboard.SetText(tag);
+                    tagInput.SendKeys(Keys.Control + "v");
+                    tagInput.SendKeys(Keys.Enter);
+                }
+
+                var completeButton = driver.FindElement(By.XPath("//*/button[@class='yt-uix-button yt-uix-button-size-default save-changes-button yt-uix-tooltip yt-uix-button-primary']/span[@class='yt-uix-button-content']"));
+                completeButton.Click();
+                Thread.Sleep(config.Page_Load);
+
+                status = true;
             }
             catch (Exception ex)
             {
@@ -86,7 +132,13 @@ namespace BetterReup.Helpers
                 }
             }
 
-            return false;
+            if (driver != null)
+            {
+                driver.Close();
+                driver.Quit();
+            }
+
+            return status;
         }
     }
 }
